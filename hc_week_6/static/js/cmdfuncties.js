7aa490d54b558b6d286f8d7826c75898dcd7f2fc
ju5tu5 <justus@ju5tu5.nl>
2012-10-12 18:03:10 +0200

    hc 6

--- hc_week_6/static/js/cmdfuncties.js
+++ hc_week_6/static/js/cmdfuncties.js
@@ -7,3 +7,26 @@ document.onkeydown = function(evt) {
 	  		break; 
 	}
 };
+
+window.onload = function(){
+	impress().init();
+}
+
+jQuery(function($){
+	
+	/*
+	var gridDimension = 5000;
+	var gridInterval = 8;
+
+	for (var x = 0; x < gridInterval+1; x++) {
+		for(var y = 0; y < gridInterval+1; y++) {
+			for(var z = 0; z < gridInterval+1; z++) {
+				$('#impress').append('<div class="step floater" data-x="'+ (-(gridDimension/2)+(x*(gridDimension/gridInterval))) +'" data-y="'+ (-(gridDimension/2)+(z*(gridDimension/gridInterval))) +'" data-z="'+ (-(gridDimension/2)+(y*(gridDimension/gridInterval))) +'"></div>');
+			};
+		};
+	};
+	*/
+	for(var i=0; i<1000; i++){
+		$('#impress').append('<div class="step floater" data-x="'+ (-4000 + Math.random()*8000) +'" data-y="'+ (-4000 + Math.random()*8000) +'" data-z="'+ (-4000 + Math.random()*8000) +'"></div>');
+	};
+});
