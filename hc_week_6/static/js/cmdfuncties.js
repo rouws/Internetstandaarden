13835b1bc26414c138c23fcf492c60a7ed9536e1
ju5tu5 <justus@ju5tu5.nl>
2012-10-14 19:38:31 +0200

    OMG college 6

--- hc_week_6/static/js/cmdfuncties.js
+++ hc_week_6/static/js/cmdfuncties.js
@@ -14,19 +14,9 @@ window.onload = function(){
 
 jQuery(function($){
 	
-	/*
-	var gridDimension = 5000;
-	var gridInterval = 8;
-
-	for (var x = 0; x < gridInterval+1; x++) {
-		for(var y = 0; y < gridInterval+1; y++) {
-			for(var z = 0; z < gridInterval+1; z++) {
-				$('#impress').append('<div class="step floater" data-x="'+ (-(gridDimension/2)+(x*(gridDimension/gridInterval))) +'" data-y="'+ (-(gridDimension/2)+(z*(gridDimension/gridInterval))) +'" data-z="'+ (-(gridDimension/2)+(y*(gridDimension/gridInterval))) +'"></div>');
-			};
-		};
-	};
-	*/
-	for(var i=0; i<1000; i++){
-		$('#impress').append('<div class="step floater" data-x="'+ (-4000 + Math.random()*8000) +'" data-y="'+ (-4000 + Math.random()*8000) +'" data-z="'+ (-4000 + Math.random()*8000) +'"></div>');
+	// Starfield
+	var border = 4000;
+	for(var i=0; i<500; i++){
+		$('#impress').append('<div class="step floater" data-x="'+ (-border + Math.random()*(border*2)) +'" data-y="'+ (-border + Math.random()*(border*2)) +'" data-z="'+ (-border + Math.random()*(border*2)) +'"></div>');
 	};
 });
